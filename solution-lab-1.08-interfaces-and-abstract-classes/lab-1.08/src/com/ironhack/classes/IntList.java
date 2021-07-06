b9603597b2e453391d998d1d8502e4237be8b7b3
Shubham Bhadani <shubham.bhadani07@gmail.com>
2021-07-06 20:15:01 +0530

    Add files via upload

---
+++ solution-lab-1.08-interfaces-and-abstract-classes/lab-1.08/src/com/ironhack/classes/IntList.java
@@ -0,0 +1,6 @@
+package com.ironhack.classes;
+
+public interface IntList {
+    void add(int value);
+    int get(int id);
+}
---
+++ solution-lab-1.08-interfaces-and-abstract-classes/lab-1.08/src/com/ironhack/classes/Sedan.java
@@ -0,0 +1,7 @@
+package com.ironhack.classes;
+
+public class Sedan extends Car {
+    public Sedan(int vinNumber, String make, String model, int mileage) {
+        super(vinNumber, make, model, mileage);
+    }
+}
